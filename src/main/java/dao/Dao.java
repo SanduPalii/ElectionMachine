21a318eef370fffe82fa9d4e02fd00a026e0fdac
SanduPalii <palii.sandu@gmail.com>
2022-03-28 10:34:30 +0300

    Dao file

---
+++ src/main/java/dao/Dao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+
+
+public class Dao {
+	private Connection conn;
+	public Dao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn=java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/vaalikone", "sandu", "password");
+		} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}		
+}
