3a763b6111d9db2ef8e6d15f0468e58b81b9e229
SanduPalii <palii.sandu@gmail.com>
2022-04-03 23:31:41 +0300

    Dao update

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -3,8 +3,6 @@ package dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.sql.PreparedStatement;
 
 
